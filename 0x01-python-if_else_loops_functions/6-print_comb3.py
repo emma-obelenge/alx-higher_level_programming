d61e8e28b978a4d8e264e56899efb1344fd4ed0f
emma-obelenge <obeleemmanuel@gmail.com>
2024-01-05 23:04:58 +0100

    complete task 6

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -2,10 +2,8 @@
 for i in range(10):
     for j in range(1, 10):
         if i > 0:
-            if i != 8 and (j + i) != 9):
+            if i != 8 and ((j + i) <= 9):
                 print(f"{i}{j + i}".format(), end=", ")
-            else:
-                break
         else:
             print(f"{i}{j}".format(), end=", ")
             continue
