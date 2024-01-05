cacc5d298ba048a264787eed497ffc0962ef1461
emma-obelenge <obeleemmanuel@gmail.com>
2024-01-06 00:54:07 +0100

    add task 8 file 8-uppercase.py

---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,9 @@
+#!usr/bin/python3
+def uppercase(str):
+    str_bank = ""
+    for char in str:
+        if ord(char) >= 97 and ord(char) < 122:
+            str_bank += chr(ord(char) - 32)
+        else:
+            str_bank += char
+    print(f"{str_bank}".format())
