bb09588a2fcca9f38dc9649aa764a018d72abac9
emma-obelenge <obeleemmanuel@gmail.com>
2024-01-05 16:00:22 -0800

    update mode of the 8-uppercase.py file

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -2,7 +2,7 @@
 def uppercase(str):
     str_bank = ""
     for char in str:
-        if ord(char) >= 97 and ord(char) < 122:
+        if ord(char) >= 97 and ord(char) <= 122:
             str_bank += chr(ord(char) - 32)
         else:
             str_bank += char
