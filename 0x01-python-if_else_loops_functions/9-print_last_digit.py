e9abfe98f7ea093444551ab34772f1320a23ce0c
emma-obelenge <obeleemmanuel@gmail.com>
2024-01-06 09:07:31 -0800

    complete task 9

--- 0x01-python-if_else_loops_functions/9-print_last_digit.py
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -1,18 +1,7 @@
-#/usr/bin/python3
+#!/usr/bin/python3
 def print_last_digit(number):
     if number < 0:
-        digit = number % -10
-        print(f"{digit}", end='')
-    elif number == 0:
-        digit = number
-        print(f"{digit}", end='')
-    else:
-        digit = number % 10
-        print(f"{digit}", end='')
+        number *= -1
+    digit = number % 10
+    print(digit, end='')
     return(digit)
-print_last_digit = __import__('9-print_last_digit').print_last_digit
-
-print_last_digit(98)
-print_last_digit(0)
-r = print_last_digit(-1024)
-print(r)
