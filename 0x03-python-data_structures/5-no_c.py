908a40ea3278a67e15a08d9645e70c95b163ee9d
emma-obelenge <obeleemmanuel@gmail.com>
2024-01-08 11:06:36 -0800

    5-no_c.py file add for task 5 of the python data struct project

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -2,7 +2,6 @@
 def no_c(my_string):
     str_cpy = ""
     for char in my_string:
-        if char == "c" or char == "C":
-            char = ""
-        str_cpy += char
-    return(str_cpy)
+        if char != "c" and char != "C":
+            str_cpy += char
+    return str_cpy
