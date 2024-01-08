c920a2805855cede96d12970109825d499610f20
emma-obelenge <obeleemmanuel@gmail.com>
2024-01-07 18:32:51 -0800

    task 2 done

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,14 +1,15 @@
 #!/usr/bin/python3
-import sys
-count = len(sys.argv)
-args = sys.argv
-index = count - 1
-if count == 1:
-    print("{} arguments.".format(index))
-elif count == 2:
-    print("{} argument:".format(index))
-    print("{}: {}".format(index, args[index]))
-else:
-    print("{} arguments:".format(index))
-    for i in range(1, count):
-        print("{}: {}".format(i, args[i]))
+if __name__ == "__main__":
+    import sys
+    count = len(sys.argv)
+    args = sys.argv
+    index = count - 1
+    if count == 1:
+        print("{} arguments.".format(index))
+    elif count == 2:
+        print("{} argument:".format(index))
+        print("{}: {}".format(index, args[index]))
+    else:
+        print("{} arguments:".format(index))
+        for i in range(1, count):
+            print("{}: {}".format(i, args[i]))
