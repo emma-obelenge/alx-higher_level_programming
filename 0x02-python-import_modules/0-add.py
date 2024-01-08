2def8190c8f5545134c2b32dad1c195d75158b19
emma-obelenge <obeleemmanuel@gmail.com>
2024-01-08 02:22:13 +0100

    update task 0 error correction

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
-import add_0 as sum
-
-a = 1
-b = 2
-
-sum.add(a, b)
+if __name__ == "__main__":
+    import add_0 as sum
+    a = 1
+    b = 2
+    result = sum.add(a, b)
+    print("{} + {} = {}".format(a, b, result))
--- 0x02-python-import_modules/add_0.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python3
-def add(a, b):
-    print("{} + {} = {}".format(a, b, a + b))
-
-
-if __name__ == "__main__":
-    a = 1
-    b = 2
-    add(a, b)
