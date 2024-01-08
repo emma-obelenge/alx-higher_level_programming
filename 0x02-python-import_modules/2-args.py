84ee536b89607d6a615c69709ef1348077ca2a55
emma-obelenge <obeleemmanuel@gmail.com>
2024-01-07 18:26:52 -0800

    task 2 done

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -7,8 +7,8 @@ if count == 1:
     print("{} arguments.".format(index))
 elif count == 2:
     print("{} argument:".format(index))
-    print("{}: {}.".format(index, args[index]))
+    print("{}: {}".format(index, args[index]))
 else:
     print("{} arguments:".format(index))
     for i in range(1, count):
-        print("{}: {}.".format(i, args[i]))
+        print("{}: {}".format(i, args[i]))
