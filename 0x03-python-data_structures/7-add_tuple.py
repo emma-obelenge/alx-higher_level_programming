eae3901e4cb8c40655a28f41cc5c8220e7ee5066
emma-obelenge <obeleemmanuel@gmail.com>
2024-01-09 00:14:30 -0800

    7-add_tuple.py file add for task 7 of the python data struct project

---
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+def add_tuple(tuple_a=(), tuple_b=()):
+    len1 = len(tuple_a)
+    len2 = len(tuple_b)
+    if len1 < 2:
+        if len1 == 0:
+            new_tuple = (0 + tuple_b[0], 0 + tuple_b[1])
+        else:
+            new_tuple = (tuple_a[0] + tuple_b[0], 0 + tuple_b[1])
+    elif len2 < 2:
+        if len2 == 0:
+            new_tuple = (tuple_a[0] + 0, tuple_a[1] + 0)
+        else:
+            new_tuple = (tuple_a[0] + tuple_b[0], tuple_a[1] + 0)
+    elif len1 >= 2 or len2 >= 2:
+        new_tuple = (tuple_a[0] + tuple_b[0], tuple_a[1] + tuple_b[1])
+    return (new_tuple)
