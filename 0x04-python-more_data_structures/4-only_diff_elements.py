fd478725ca24b6025a76358a31d78e8451a689a0
emma-obelenge <obeleemmanuel@gmail.com>
2024-01-11 20:19:39 -0800

    update 4-only_diff_elements.py to conform with pep8 standards. python more struct project

--- 0x04-python-more_data_structures/4-only_diff_elements.py
+++ 0x04-python-more_data_structures/4-only_diff_elements.py
@@ -2,4 +2,4 @@
 def only_diff_elements(set_1, set_2):
     in_both = set_1.intersection(set_2)
     combined = set_1.union(set_2)
-    return(combined.difference(in_both))
+    return (combined.difference(in_both))
