100986a2ea2507ddf7ffea9305ac5c3015a51c1a
emma-obelenge <obeleemmanuel@gmail.com>
2024-01-12 15:53:57 -0800

    6-print_sorted_dictionary.py file add for task 6 of the python more data struct project

--- 0x04-python-more_data_structures/6-print_sorted_dictionary.py
+++ 0x04-python-more_data_structures/6-print_sorted_dictionary.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
 def print_sorted_dictionary(a_dictionary):
-  result = dict(sorted(a_dictionary.items()))
-  print(result)
+    result = sorted(a_dictionary.items())
+    for key, value in result:
+        print("{}: {}".format(key, value))
