8aea3108c7f1a4a8a4386cf23210dca8fe0f6e5a
emma-obelenge <obeleemmanuel@gmail.com>
2024-01-12 16:20:11 -0800

    7-update_dictionary.py add for task 7 of the python more data struct project

---
+++ 0x04-python-more_data_structures/7-update_dictionary.py
@@ -0,0 +1,7 @@
+def update_dictionary(a_dictionary, key, value):
+    new_dict = a_dictionary.copy()
+    if key in new_dict:
+        new_dict[key] = value
+    else:
+        new_dict.update({key: value})
+        return (new_dict)
