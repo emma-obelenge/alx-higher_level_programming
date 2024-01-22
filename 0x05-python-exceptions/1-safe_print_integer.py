8d17ecb5619fa3b09a1ce31d537a8bf2e23b4de3
emma-obelenge <obeleemmanuel@gmail.com>
2024-01-22 21:15:27 +0100

    add 1-safe_print_integer.py file for task 1 of the python exceptions project

---
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+def safe_print_integer(value):
+    try:
+        int_value = int(value)
+        print("{:d}".format(value))
+        return (True)
+    except (ValueError, TypeError):
+        return (False)
