46f4a29ce4adaba0baba49bb7c0296caf7a5d96a
emma-obelenge <obeleemmanuel@gmail.com>
2024-01-23 23:50:02 +0100

    1-square.py file add for task 1 of the python classes project

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""Define a class Square."""
+
+
+class Square:
+    """A Square class, for handling values needed for computation."""
+    def __init__(self, size):
+        """__init function handles instantiation with size"""
+        self.__size = size
