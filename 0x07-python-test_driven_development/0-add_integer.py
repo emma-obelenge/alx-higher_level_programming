f1e6893d13fc20ece70ed1ddd81e7225afed1b4c
emma-obelenge <obeleemmanuel@gmail.com>
2024-01-29 21:04:06 +0100

    0-add_integer.py file add for task 0 of the pyton TDD project

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -1,9 +1,17 @@
 #!/usr/bin/python3
-"""
-custom function that adds two imtegers
-"""
+"""custom function that adds two integers."""
 
 
 def add_integer(a, b=98):
-	"""adds integers a and b"""
-	pass
+    """Returns the addition of a and b integers.
+
+    Float args are typecasted to ints before any addition.
+
+    Raises:
+        TypeError: when a or b is a non-int or non-float.
+    """
+    if not isinstance(a, (int, float)):
+        raise TypeError("a must be an integer")
+    if not isinstance(b, (int, float)):
+        raise TypeError("b must be an integer")
+    return (int(a) + int(b))
