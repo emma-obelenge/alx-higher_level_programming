f702eb7feb7e0ad155ab0c8822de601bb1b3bdef
emma-obelenge <obeleemmanuel@gmail.com>
2024-02-05 21:31:12 +0100

    4-inherits_from.py file add for task 4 of the python inheritance project

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -2,20 +2,19 @@
 """Here we would design a function that returns True of False \
     upon checking whether an object is an instance of a \
         class that is inherited directly or indirectly from \
-            a specified class
-"""
+            a specified class."""
 
 
 def inherits_from(obj, a_class):
     """Checks if obj is an instance of a_class of a class \
-        instance, directly or indirectly
+        instance, directly or indirectly.
 
         Args:
-            obj (any): The object to be checked
-            a_class (type): The class for check comparism
+            obj (any): The object to be checked.
+            a_class (type): The class for check comparism.
         Returns:
-            True if obj is an instance of a_class
-            and False if the reverse is the case
+            True if obj is an instance of a_class.
+            and False if the reverse is the case.
     """
     if (issubclass(type(obj), a_class) and type(obj)) != a_class:
         return (True)
