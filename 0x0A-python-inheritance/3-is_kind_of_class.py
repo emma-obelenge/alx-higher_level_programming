ac4b2f3952bc53b6ffd18e8748a9e0d1dffa24f3
emma-obelenge <obeleemmanuel@gmail.com>
2024-02-05 21:05:38 +0100

    3-is_kind_of_class.py file add for task 3 of the python inheritance project

--- 0x0A-python-inheritance/3-is_kind_of_class.py
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -4,12 +4,12 @@ a class or class that inherits from another class"""
 
 
 def is_kind_of_class(obj, a_class):
+    """Checks if obj is an instance of a_class, or of class of class
+        Args:
+            obj (any): The object to be checked
+            a_class (type): The class for check comparism
+        Returns:
+            True if obj is an instance of a_class
+            and False if the reverse is the case
+    """
     return (isinstance(obj, a_class))
-
-a = 1
-if is_kind_of_class(a, int):
-    print("{} comes from {}".format(a, int.__name__))
-if is_kind_of_class(a, float):
-    print("{} comes from {}".format(a, float.__name__))
-if is_kind_of_class(a, object):
-    print("{} comes from {}".format(a, object.__name__))
