4373c59de3c365f3904c7a2d2e8cecbe690240fe
emma-obelenge <obeleemmanuel@gmail.com>
2024-02-05 22:11:39 +0100

    add 6-base_geometry.py file for task 6 of the python inheritance project

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""An empty class"""
+
+
+class BaseGeometry:
+    """An empty class BaseGeometry"""
+    def area(self):
+        """area function that only raises an exception"""
+        raise Exception("area() is not implemented")
