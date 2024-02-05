39bdce5f97ac1b81ccb876a40b9f8fdf0912d11e
emma-obelenge <obeleemmanuel@gmail.com>
2024-02-05 21:11:20 +0100

    3-is_kind_of_class.py file add for task 3 of the python inheritance project

--- 0x0A-python-inheritance/3-is_kind_of_class.py
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -12,4 +12,7 @@ def is_kind_of_class(obj, a_class):
             True if obj is an instance of a_class
             and False if the reverse is the case
     """
-    return (isinstance(obj, a_class))
+    if (isinstance(obj, a_class)):
+        return (True)
+    else:
+        return (False)
