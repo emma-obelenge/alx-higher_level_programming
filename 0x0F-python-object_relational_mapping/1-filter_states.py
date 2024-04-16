d16cae7bdbece7cde81e1f4eca2a1163982a3315
emma-obelenge <obelemmanuel@gmail.com>
2024-04-16 10:37:36 -0700

    update 1-filter_states.py file for pycodestyle and enhance readability

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -6,15 +6,20 @@ from the database hbtn_0e_0_usa
 import MySQLdb
 from sys import argv
 
-
 if __name__ == "__main__":
-    db = MySQLdb.connect(host="localhost", port=3306,
-                         user=argv[1], passwd=argv[2], db=argv[3])
-    cur = db.cursor()
-    cur.execute("SELECT * FROM states ORDER BY states.id")
-    rows = cur.fetchall()
+    db = MySQLdb.connect(
+        host="localhost",
+        port=3306,
+        user=argv[1],
+        passwd=argv[2],
+        db=argv[3]
+    )
+    cursor = db.cursor()
+    cursor.execute("SELECT * FROM states ORDER BY states.id")
+
+    rows = cursor.fetchall()
     for row in rows:
         if row[1][0] == 'N':
             print(row)
-    cur.close()
-    db.close()
+        cursor.close()
+        db.close()
