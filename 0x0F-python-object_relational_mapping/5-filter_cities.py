e08e71f327fa0cf60ca8a9e79e04bf1824cc5ac9
emma-obelenge <obelemmanuel@gmail.com>
2024-04-17 06:58:50 -0700

    5-filter_cities.py file update for compatibility issues and error value printing

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -8,20 +8,26 @@ from sys import argv
 
 
 if __name__ == "__main__":
-    db = MySQLdb.connect(host="localhost", port=3306,
-                         user=argv[1], passwd=argv[2], db=argv[3])
-    cur = db.cursor()
-    sql = "SELECT cities.name FROM cities JOIN states ON\
+    db = MySQLdb.connect(
+        host="localhost",
+        port=3306,
+        user=argv[1],
+        passwd=argv[2],
+        db=argv[3]
+    )
+
+    cursor = db.cursor()
+    sql_cmd = "SELECT cities.name FROM cities JOIN states ON\
     cities.state_id = states.id WHERE states.name=%s\
     ORDER BY cities.id"
-    num_rows = cur.execute(sql, (argv[4],))
-    rows = cur.fetchall()
+    num_rows = cursor.execute(sql_cmd, (argv[4],))
+    rows = cursor.fetchall()
     result = []
     i = 0
     for row in rows:
-        result.append(rows[i][0])
+        result.append(row[0])
         i += 1
     joined = ", ".join(result)
     print(joined)
-    cur.close()
+    cursor.close()
     db.close()
