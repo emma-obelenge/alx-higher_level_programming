794b9b940c0f78ca3a343937b2b978ab9cb138f3
emma-obelenge <obelemmanuel@gmail.com>
2024-04-16 22:49:13 -0700

    modify 4-cities_by_state.py file for readability, efficiency and pycodestyle compliance

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -7,13 +7,19 @@ from sys import argv
 
 
 if __name__ == "__main__":
-    db = MySQLdb.connect(host="localhost", port=3306,
-                         user=argv[1], passwd=argv[2], db=argv[3])
-    cur = db.cursor()
-    sql = "SELECT cities.id, cities.name, states.name FROM cities\
+    db = MySQLdb.connect(
+        host="localhost",
+        port=3306,
+        user=argv[1],
+        passwd=argv[2],
+        db=argv[3]
+    )
+
+    cursor = db.cursor()
+    sql_cmd = "SELECT cities.id, cities.name, states.name FROM cities\
     JOIN states ON cities.state_id = states.id ORDER BY cities.id"
-    num_rows = cur.execute(sql)
-    for i in range(num_rows):
-        print(cur.fetchone())
-    cur.close()
+    rows = cursor.execute(sql_cmd)
+    for i in range(rows):
+        print(cursor.fetchone())
+    cursor.close()
     db.close()
